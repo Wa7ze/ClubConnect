e2e4d298d4cfd0f29afd77eb3b36e91e00099a0b
Wa7ze <mmazen0132@yahoo.com>
2025-09-10 10:18:30 +0300

    Simplify deployment: Use SQLite and compatible Pillow version, remove runtime.txt

--- ClubConnect/settings_production.py
+++ ClubConnect/settings_production.py
@@ -18,6 +18,15 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'))
 }
 
+# Use SQLite if no DATABASE_URL is provided
+if not os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
